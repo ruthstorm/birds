80207f66adfe9ccef1e2a4777d653ec1d6f1be7c
ruthstorm <1965071+ruthstorm@users.noreply.github.com>
2026-03-25 13:07:11 -0700

    Using GitHub webflow to set up GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/birds/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
